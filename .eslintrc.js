4e5ad262b6e749e4c46a5b302377a86f40d8b6c7
Mateusz Michałowski <mateusz.michalowski@monterail.com>
2025-03-19 13:51:07 +0100

    temporary tests removal

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,7 @@
 module.exports = {
     parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint', 'jest'],
-    extends: [
-        'plugin:@typescript-eslint/recommended',
-        'plugin:jest/recommended'
-    ],
+    plugins: [],
+    extends: ['plugin:@typescript-eslint/recommended'],
     rules: {
         '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/explicit-function-return-type': ['error', {
