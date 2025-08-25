1b2980d00e3f00c5cb2f41b560c6dd169a5230e7
Mateusz Michałowski <mateusz.michalowski@monterail.com>
2025-08-25 12:34:42 +0200

    fix lint

--- src/lighthouse.ts
+++ src/lighthouse.ts
@@ -57,7 +57,7 @@ async function runLighthouseForUrl(
 
     const sanitizedChromeFlags = chromeFlags.replace(/"/g, '\\"').replace(/;/g, '');
 
-    let effectiveThrottlingMethod = throttlingMethod;
+    const effectiveThrottlingMethod = throttlingMethod;
     let cpuThrottlingArgs = '';
     
     if (disableCpuThrottling) {
